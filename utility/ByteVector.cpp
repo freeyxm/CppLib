7a3b39b8b626561b4a97320c5574d1ef4bb5efd2
freeyxm <freeyxm@live.com>
2021-09-28 18:13:17 +0800

    Update ByteVector.

--- utility/ByteVector.cpp
+++ utility/ByteVector.cpp
@@ -1,2 +1,44 @@
 #include "stdafx.h"
 #include "ByteVector.h"
+
+bool ByteVector::replace(size_t index, const std::vector<byte>& bytes)
+{
+	if (index + bytes.size() > m_bytes.size())
+	{
+		return false;
+	}
+	auto it = m_bytes.begin() + index;
+	for (size_t i = 0; i < bytes.size(); ++i)
+	{
+		*it++ = bytes[i];
+	}
+	return true;
+}
+
+bool ByteVector::replace(size_t index, const std::string& str)
+{
+	if (index + str.size() > m_bytes.size())
+	{
+		return false;
+	}
+	auto it = m_bytes.begin() + index;
+	for (size_t i = 0; i < str.size(); ++i)
+	{
+		*it++ = str[i];
+	}
+	return true;
+}
+
+bool ByteVector::replace(size_t index, const char* str, const size_t size)
+{
+	if (index + size > m_bytes.size())
+	{
+		return false;
+	}
+	auto it = m_bytes.begin() + index;
+	for (size_t i = 0; i < size; ++i)
+	{
+		*it++ = str[i];
+	}
+	return true;
+}
--- utility/ByteVector.h
+++ utility/ByteVector.h
@@ -4,7 +4,7 @@
 #include <limits>
 #include "BitConverter.h"
 
-typedef char byte;
+typedef unsigned char byte;
 
 class ByteVector
 {
@@ -13,64 +13,118 @@ public:
 	{
 		m_bytes.push_back(value);
 	}
-
 	inline void push_back(bool value)
 	{
 		m_bytes.push_back(value);
 	}
-
 	inline void push_back(int16_t value)
 	{
 		BitConverter::GetBytes(value, m_bytes);
 	}
-
 	inline void push_back(int32_t value)
 	{
 		BitConverter::GetBytes(value, m_bytes);
 	}
-
 	inline void push_back(int64_t value)
 	{
 		BitConverter::GetBytes(value, m_bytes);
 	}
-
 	inline void push_back(uint16_t value)
 	{
 		BitConverter::GetBytes(value, m_bytes);
 	}
-
 	inline void push_back(uint32_t value)
 	{
 		BitConverter::GetBytes(value, m_bytes);
 	}
-
 	inline void push_back(uint64_t value)
 	{
 		BitConverter::GetBytes(value, m_bytes);
 	}
-
 	inline void push_back(float value)
 	{
 		BitConverter::GetBytes(value, m_bytes);
 	}
-
 	inline void push_back(double value)
 	{
 		BitConverter::GetBytes(value, m_bytes);
 	}
-
 	inline void push_back(const std::string& str)
 	{
 		m_bytes.insert(m_bytes.end(), str.cbegin(), str.cend());
 	}
-
 	inline void push_back(const char* str, const size_t size)
 	{
 		m_bytes.insert(m_bytes.end(), str, str + size);
 	}
 
+	bool replace(size_t index, char value)
+	{
+		if (index + 1 > m_bytes.size()) return false;
+		auto it = m_bytes.begin() + index;
+		*it = value;
+		return true;
+	}
+	bool replace(size_t index, bool value)
+	{
+		if (index + 1 > m_bytes.size()) return false;
+		auto it = m_bytes.begin() + index;
+		*it = value;
+		return true;
+	}
+	bool replace(size_t index, int16_t value)
+	{
+		const auto& bytes = BitConverter::GetBytes(value);
+		return replace(index, bytes);
+	}
+	bool replace(size_t index, int32_t value)
+	{
+		const auto& bytes = BitConverter::GetBytes(value);
+		return replace(index, bytes);
+	}
+	bool replace(size_t index, int64_t value)
+	{
+		const auto& bytes = BitConverter::GetBytes(value);
+		return replace(index, bytes);
+	}
+	bool replace(size_t index, uint16_t value)
+	{
+		const auto& bytes = BitConverter::GetBytes(value);
+		return replace(index, bytes);
+	}
+	bool replace(size_t index, uint32_t value)
+	{
+		const auto& bytes = BitConverter::GetBytes(value);
+		return replace(index, bytes);
+	}
+	bool replace(size_t index, uint64_t value)
+	{
+		const auto& bytes = BitConverter::GetBytes(value);
+		return replace(index, bytes);
+	}
+	bool replace(size_t index, float value)
+	{
+		const auto& bytes = BitConverter::GetBytes(value);
+		return replace(index, bytes);
+	}
+	bool replace(size_t index, double value)
+	{
+		const auto& bytes = BitConverter::GetBytes(value);
+		return replace(index, bytes);
+	}
+	bool replace(size_t index, const std::string& str);
+	bool replace(size_t index, const char* str, const size_t size);
+
+	inline size_t size() const { return m_bytes.size(); }
+
+	inline std::vector<byte>& Bytes() { return m_bytes; }
+	inline const std::vector<byte>& Bytes() const { return m_bytes; }
+
 	operator std::vector<byte>& () { return m_bytes; }
 	operator const std::vector<byte>& () const { return m_bytes; }
+
+private:
+	bool replace(size_t index, const std::vector<byte>& bytes);
 
 private:
 	std::vector<byte> m_bytes;
